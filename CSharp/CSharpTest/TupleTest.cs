088a0c14b2ae8267a9a4cbc55951c560ee05268b
Matthew Hanna <irtheman@gmail.com>
2020-10-13 09:22:28 -0400

    Starting CSharp Ray Tracer Challenge

---
+++ CSharp/CSharpTest/TupleTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class TupleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
