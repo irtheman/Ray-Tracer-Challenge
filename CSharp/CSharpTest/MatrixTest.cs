8151f8e30c3f8aa7539dc02808d3c2e477365fb3
Matthew Hanna <irtheman@gmail.com>
2020-11-02 16:26:42 -0500

    Matrix testing in progress

--- CSharp/CSharpTest/MatrixTest.cs
+++ CSharp/CSharpTest/MatrixTest.cs
@@ -9,11 +9,11 @@ namespace CSharpTest
         [TestMethod]
         public void TestMatrixCreate4x4()
         {
-            var m = Matrix(4, 4, new double[] {
-                           1, 2, 3, 4,
-                           5.5, 6.5, 7.5, 8.5,
-                           9, 10, 11, 12,
-                           13.5, 14.5, 15.5, 16.5 });
+            var m = new Matrix(4, 4, new double[] {
+                               1, 2, 3, 4,
+                               5.5, 6.5, 7.5, 8.5,
+                               9, 10, 11, 12,
+                               13.5, 14.5, 15.5, 16.5 });
 
             Assert.AreEqual(m[0, 0], 1);
             Assert.AreEqual(m[0, 3], 4);
@@ -27,7 +27,7 @@ namespace CSharpTest
         [TestMethod]
         public void TestMatrixCreate2x2()
         {
-            var m = Matrix(2, 2);
+            var m = new Matrix(2, 2);
             m[0, 0] = -3;
             m[0, 1] = 5;
             m[1, 0] = 1;
@@ -42,10 +42,10 @@ namespace CSharpTest
         [TestMethod]
         public void TestMatrixCreate3x3()
         {
-            var m = Matrix(4, 4, new double[] {
-                           -3, 5, 0,
-                           1, -2, -7,
-                           0, 1, 1 });
+            var m = new Matrix(3, 3, new double[] {
+                               -3, 5, 0,
+                               1, -2, -7,
+                               0, 1, 1 });
 
             Assert.AreEqual(m[0, 0], -3);
             Assert.AreEqual(m[1, 1], -2);
@@ -55,17 +55,17 @@ namespace CSharpTest
         [TestMethod]
         public void TestMatrixEqualityTrue()
         {
-            var m1 = Matrix(4, 4, new double[] {
-                            1, 2, 3, 4,
-                            5, 6, 7, 8,
-                            9, 8, 7, 6,
-                            5, 4, 3, 2 });
+            var m1 = new Matrix(4, 4, new double[] {
+                                1, 2, 3, 4,
+                                5, 6, 7, 8,
+                                9, 8, 7, 6,
+                                5, 4, 3, 2 });
 
-            var m2 = Matrix(4, 4, new double[] {
-                            1, 2, 3, 4,
-                            5, 6, 7, 8,
-                            9, 8, 7, 6,
-                            5, 4, 3, 2 });
+            var m2 = new Matrix(4, 4, new double[] {
+                                1, 2, 3, 4,
+                                5, 6, 7, 8,
+                                9, 8, 7, 6,
+                                5, 4, 3, 2 });
 
             Assert.AreEqual(m1, m2);
         }
@@ -73,19 +73,81 @@ namespace CSharpTest
         [TestMethod]
         public void TestMatrixEqualityFalse()
         {
-            var m1 = Matrix(4, 4, new double[] {
-                            1, 2, 3, 4,
-                            5, 6, 7, 8,
-                            9, 8, 7, 6,
-                            5, 4, 3, 2 });
+            var m1 = new Matrix(4, 4, new double[] {
+                                1, 2, 3, 4,
+                                5, 6, 7, 8,
+                                9, 8, 7, 6,
+                                5, 4, 3, 2 });
 
-            var m2 = Matrix(4, 4, new double[] {
-                            2, 3, 4, 5,
-                            6, 7, 8, 9,
-                            8, 7, 6, 5,
-                            4, 3, 2, 1 });
+            var m2 = new Matrix(4, 4, new double[] {
+                                2, 3, 4, 5,
+                                6, 7, 8, 9,
+                                8, 7, 6, 5,
+                                4, 3, 2, 1 });
 
             Assert.AreNotEqual(m1, m2);
+        }
+
+        [TestMethod]
+        public void TestMatrixMultiplyScalar()
+        {
+            var m1 = new Matrix(4, 4, new double[] {
+                                1, 2, 3, 4,
+                                5, 6, 7, 8,
+                                9, 8, 7, 6,
+                                5, 4, 3, 2 });
+
+            var m2 = new Matrix(4, 4, new double[] {
+                                2, 4, 6, 8,
+                                10, 12, 14, 16,
+                                18, 16, 14, 12,
+                                10, 8, 6, 4 });
+
+            Assert.AreEqual(m1 * 2, m2);
+        }
+
+        [TestMethod]
+        public void TestMatrixMultiply()
+        {
+            var m1 = new Matrix(4, 4, new double[] {
+                                1, 2, 3, 4,
+                                5, 6, 7, 8,
+                                9, 8, 7, 6,
+                                5, 4, 3, 2 });
+
+            var m2 = new Matrix(4, 4, new double[] {
+                                2, 4, 6, 8,
+                                10, 12, 14, 16,
+                                18, 16, 14, 12,
+                                10, 8, 6, 4 });
+
+            var result = new Matrix(4, 4, new double[] {
+                                    2, 4, 6, 8,
+                                    10, 12, 14, 16,
+                                    18, 16, 14, 12,
+                                    10, 8, 6, 4 });
+
+            Assert.AreEqual(m1 * m2, result);
+        }
+
+        [TestMethod]
+        public void TestMatrixMultiplyTuple()
+        {
+            var m = new Matrix(4, 4, new double[] {
+                               1, 2, 3, 4,
+                               5, 6, 7, 8,
+                               9, 8, 7, 6,
+                               5, 4, 3, 2 });
+
+            var t = new Tuple(1, 2, 3, 4);
+
+            var result = new Matrix(4, 4, new double[] {
+                                    2, 4, 6, 8,
+                                    10, 12, 14, 16,
+                                    18, 16, 14, 12,
+                                    10, 8, 6, 4 });
+
+            Assert.AreEqual(m * t, result);
         }
     }
 }
---
+++ CSharp/Matrix.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CSharp
+{
+    public class Matrix
+    {
+        private readonly double[,] m;
+
+        public Matrix() : this(4, 4)
+        {
+            // Nothing here
+        }
+
+        public Matrix(int rows, int columns) : this(rows, columns, null)
+        {
+            // Nothing here
+        }
+
+        public Matrix(int rows, int columns, double[] data)
+        {
+            m = new double[rows, columns];
+            LoadMatrix(rows, columns, data);
+        }
+
+        public Matrix(Matrix matrix)
+        {
+            m = new double[matrix.Rows, matrix.Columns];
+
+            for (int i = 0; i < matrix.Rows; i++)
+                for (int j = 0; j < matrix.Columns; j++)
+                    m[i, j] = matrix[i, j];
+        }
+
+        public Matrix(Tuple t, bool isColumn = true)
+        {
+            int rows = 1;
+            int columns = 1;
+
+            if (isColumn)
+                rows = 4;
+            else
+                columns = 4;
+
+            double[] data = { t.x, t.y, t.z, t.w };
+
+            m = new double[rows, columns];
+            LoadMatrix(rows, columns, data);
+        }
+
+        public int Rows => m.GetUpperBound(0) + 1;
+        public int Columns => m.GetUpperBound(1) + 1;
+        
+        public double this[int row, int column]
+        {
+            get
+            {
+                return m[row, column];
+            }
+
+            set
+            {
+                m[row, column] = value;
+            }
+        }
+
+        public static Matrix operator*(Matrix lhs, double rhs)
+        {
+            Matrix ret = new Matrix(lhs);
+
+            for (int i = 0; i < lhs.Rows; i++)
+                for (int j = 0; j < lhs.Columns; j++)
+                    ret[i, j] *= rhs;
+
+            return ret;
+        }
+
+        public static Matrix operator*(Matrix lhs, Matrix rhs)
+        {
+            if (lhs.Columns != rhs.Rows)
+                throw new Exception("Matrix Cannot Be Multiplied.");
+
+            Matrix ret = new Matrix(lhs.Rows, rhs.Columns);
+            double temp = 0.0;
+
+            for (int i = 0; i < lhs.Rows; i++)
+            {
+                for (int j = 0; j < rhs.Columns; j++)
+                {
+                    temp = 0;
+                    for (int k = 0; k < lhs.Columns; k++)
+                        temp += lhs[i, k] * rhs[k, j];
+                    ret[i, j] = temp;
+                }
+            }
+
+            return ret;
+        }
+
+        public static Matrix operator *(Matrix lhs, Tuple rhs)
+        {
+            if ((lhs.Rows != 4) || (lhs.Columns != 4))
+                throw new Exception("Matrix must be Nx4 or 4xM.");
+
+            return lhs * new Matrix(rhs, lhs.Rows == 4);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Matrix;
+            if ((other == null) ||
+                (Rows != other.Rows) ||
+                (Columns != other.Columns))
+                return false;
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Columns; j++)
+                    if (m[i, j] != other[i, j])
+                        return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return m.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Rows}x{Columns} [");
+
+            for (int i = 0; i < Rows; i++)
+            {
+                sb.Append("[");
+
+                for (int j = 0; j < Columns; j++)
+                {
+                    sb.Append($"{m[i, j]} ");
+                }
+
+                sb.Append("] ");
+            }
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+
+        private void LoadMatrix(int rows, int columns, double[] data)
+        {
+            int index = 0;
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    m[i, j] = (data != null) && (index < data.Length) ? data[index++] : 0;
+        }
+    }
+}
